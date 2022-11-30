b51f53f15bc3f10a576c4ff2ffcef6f2d4496d3e
Claudio Bisegni <bisegni@slac.stanford.edu>
2022-11-29 16:23:52 -0800

    baseline

---
+++ launch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+PLATFORM_DETECTED=`uname -m`
+export PATH=$PATH:/usr/local/epics/base/bin/linux-$PLATFORM_DETECTED
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/epics/base/lib/linux-$PLATFORM_DETECTED
+softIoc "$@"
